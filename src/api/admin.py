4b37d92304b1bdae37418b3a7844d82b5ae6bc96
Maikel Contramaestre <116273585+BlackNereus@users.noreply.github.com>
2024-12-27 17:25:26 +0000

    feat: base de datos maikel

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Empresa, Servicio, GestorCitas 
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,9 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Empresa, db.session))
+    admin.add_view(ModelView(Servicio, db.session))
+    admin.add_view(ModelView(GestorCitas, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -3,9 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    name = db.Column(db.String(100))
+    empresa_id = db.Column(db.Integer, db.ForeignKey('empresas.id'))
+    empresa = db.relationship('Empresa', backref=('users'), lazy=True)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -17,3 +21,56 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+class Empresa (db.Model):
+    __tablename__ = 'empresas'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    servicios = db.relationship('Servicio', backref='empresa', lazy=True)
+
+    def __repr__(self):
+        return f'<Empresa{self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+        }
+
+
+class Servicio(db.Model):
+    __tablename__ = 'servicios'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    empresa_id = db.Column(db.Integer, db.ForeignKey('empresas.id'))
+    
+
+    def __repr__(self):
+        return f'<Servicio {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "empresa_id": self.empresa_id,
+        }
+
+class GestorCitas(db.Model):
+    __tablename__ = 'gestor_citas'
+    id = db.Column(db.Integer, primary_key =True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    servicio_id = db.Column(db.Integer, db.ForeignKey('servicios.id'))
+    fecha = db.Column(db.DateTime)
+    user = db.relationship('User', backref=('gestor_citas'))
+    servicio = db.relationship('Servicio', backref =('gestor_citas'), lazy=True )
+
+    def __repr__(self):
+        return f'<GestorCitas {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "servicio_id": self.servicio_id,
+        }
+        
