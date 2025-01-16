04dfc68f6fdae4bb0c988c3e7a182cc0fb97b121
Elandruf <andreseduardo2710@gmail.com>
2025-01-16 12:27:48 +0000

    navbar fixed

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export const Navbar = () => {
 	return (
 		
-		<nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
 			<div className="container-fluid justify-content-center" style={{background: "#2C5364"}}>
 				<a className="navbar-brand" href="#">Logo</a>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
