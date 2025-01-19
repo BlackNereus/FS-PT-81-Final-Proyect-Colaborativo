3262e94c7bd1f9b9224fb925d1c2d3569ed47c6a
Elandruf <andreseduardo2710@gmail.com>
2025-01-19 12:30:08 +0000

    Cambios en login

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,43 +1,24 @@
+
 import React from "react";
+import { useNavigate } from "react-router-dom"; 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 export const Navbar = () => {
-	return (
+  const navigate = useNavigate(); 
 
-		// <nav class="navbar navbar-expand-lg bg-body-tertiary">
-		// 	<div class="container-fluid">
-		// 		<a class="navbar-brand" href="#">Navbar</a>
-		// 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-		// 			<span class="navbar-toggler-icon"></span>
-		// 		</button>
-		// 		<div class="collapse navbar-collapse" id="navbarNav">
-		// 			<ul class="navbar-nav">
-		// 				<li class="nav-item">
-		// 					<a class="nav-link active" aria-current="page" href="#">Prices</a>
-		// 				</li>
-		// 				<li class="nav-item">
-		// 					<a class="nav-link" href="#">Functionabilities</a>
-		// 				</li>
-		// 				<li class="nav-item">
-		// 					<a class="nav-link" href="#">Contact</a>
-		// 				</li>
-		// 				<li class="nav-item">
-		// 					<a class="nav-link" href="#">Noticies</a>
-		// 				</li>
-		// 			</ul>
-		// 		</div>
-		// 		</nav>
+  const handlePruebaYa = () => {
+    navigate("/login"); 
+  };
 
-
-		 <nav class="navbar d-flex">
-			<div class="container">
-				<button class="btn-green">PRUEBA</button>
-				<a className="nav-link active" style={{ color: "white" }} aria-current="#" href="#">Precio</a>
-				<a className="nav-link" style={{ color: "white" }} href="#">Funcionabilidades</a>
-				<a className="nav-link" style={{ color: "white" }} href="#">Contacto</a>
-				<a className="nav-link" style={{ color: "white" }} href="#">Noticias</a>
-			</div>
-		</nav> 
-	)
-}
+  return (
+    <nav className="navbar d-flex">
+      <div className="container">
+        <button onClick={handlePruebaYa} className="btn-green">Prueba ya</button>
+        <a className="nav-link active" style={{ color: "white" }} aria-current="#" href="#">Precio</a>
+        <a className="nav-link" style={{ color: "white" }} href="#">Funcionabilidades</a>
+        <a className="nav-link" style={{ color: "white" }} href="#">Contacto</a>
+        <a className="nav-link" style={{ color: "white" }} href="#">Noticias</a>
+      </div>
+    </nav>
+  );
+};
