12dc51c74743cb017087ec070b9fbb353bfc90a4
Elandruf <andreseduardo2710@gmail.com>
2025-01-20 09:50:44 +0000

    limpieza dbase

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,4 @@
-git branch -andfrom flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,30 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export const Navbar = () => {
 	return (
 
-		// <nav class="navbar navbar-expand-lg bg-body-tertiary">
-		// 	<div class="container-fluid">
-		// 		<a class="navbar-brand" href="#">Navbar</a>
-		// 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-		// 			<span class="navbar-toggler-icon"></span>
-		// 		</button>
-		// 		<div class="collapse navbar-collapse" id="navbarNav">
-		// 			<ul class="navbar-nav">
-		// 				<li class="nav-item">
-		// 					<a class="nav-link active" aria-current="page" href="#">Prices</a>
-		// 				</li>
-		// 				<li class="nav-item">
-		// 					<a class="nav-link" href="#">Functionabilities</a>
-		// 				</li>
-		// 				<li class="nav-item">
-		// 					<a class="nav-link" href="#">Contact</a>
-		// 				</li>
-		// 				<li class="nav-item">
-		// 					<a class="nav-link" href="#">Noticies</a>
-		// 				</li>
-		// 			</ul>
-		// 		</div>
-		// 		</nav>
-
+		
 
 		 <nav class="navbar d-flex">
 			<div class="container">
