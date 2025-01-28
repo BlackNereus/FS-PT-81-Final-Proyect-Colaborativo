8141177b805dae1ae12615efc81d3471fbbde292
JoanSimonutti <joansimonutticode@gmail.com>
2025-01-28 13:27:10 +0000

    agrego el logo del navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -15,7 +15,7 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
       <div className="container-fluid justify-content-center">
-        <a className="navbar-brand" href="#"><img src={ `../public/logo.png` } alt="LOGO" /></a>
+        <a className="navbar-brand" href="#"><img src={ `../public/logonavbar.png` } alt="CLINICA CENTRAL" /></a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
