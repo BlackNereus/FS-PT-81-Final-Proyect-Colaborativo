5e0462bf386da84a51b98720a06e838c4661aa10
JoanSimonutti <joansimonutticode@gmail.com>
2025-01-28 21:46:27 +0000

    Jumbotron 2.0

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -76,11 +76,11 @@ export const Home = () => {
 					<div className="d-flex flex-column justify-content-center align-items-center my-4 divdelfondo">
 						<div className="d-flex flex-column justify-content-center align-items-center my-4">
 							<h1>AGENDA AHORA TU CITA EN CLINICA CENTRAL</h1>
-							<h3>Es fácil, rápido y seguro.</h3>
+							<h3>Es fácil, rápido y seguro</h3>
 						</div>
 						<div class="d-grid gap-2 col-3 mx-auto">
 						<button className="btn btn-success btn-lg" onClick={handleAgendarCita}>
-							RESERVA TU CITA
+							RESERVA TU CITA 
 						</button>
 						</div>
 					</div>
